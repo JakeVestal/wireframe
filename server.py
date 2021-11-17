57b9c25e84bb3ec9e2f045f87b49febdce76d0fb
JakeVestal <jmvestal@gmail.com>
2021-11-17 12:37:05 -0500

    commit

--- server.py
+++ server.py
@@ -1,11 +1,5 @@
 # Test line
 from waitress import serve
 from wireframe import app
-from dash_auth import BasicAuth
 
-VALID_USERNAME_PWD_PAIR = open('login_creds.txt').readlines()[0].split(':')
-
-auth = BasicAuth(app, {VALID_USERNAME_PWD_PAIR[0]: VALID_USERNAME_PWD_PAIR[1]})
-app.config['suppress_callback_exceptions']=True
-
-serve(app.server, port=3000)
+serve(app.server, host='localhost', port=3000)
